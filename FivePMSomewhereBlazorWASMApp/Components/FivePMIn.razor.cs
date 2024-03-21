736e83a06a8da00239850924b25ed85c1c81c1e8
John Tomlinson <john.tomlinson@teamsynergi.co.uk>
2024-03-21 11:36:47 +0000

    Improved display and added an About page

---
+++ FivePMSomewhereBlazorWASMApp/Components/FivePMIn.razor.cs
@@ -0,0 +1,20 @@
+﻿using FivePMSomewhereShared.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace FivePMSomewhereBlazorWASMApp.Components
+{
+    public partial class FivePMIn
+    {
+        [Parameter]
+        public TargetTimeModel? CurrentTimeZone { get; set; }
+
+        [Parameter]
+        public TimeAfterTargetModel? PreviousTimeZone { get; set; }
+
+        private string? Country => CurrentCountry ?? PreviousCountry;
+
+        private string? CurrentCountry => CurrentTimeZone?.RandomCountry;
+
+        private string? PreviousCountry => PreviousTimeZone?.RandomCountry;
+    }
+}
