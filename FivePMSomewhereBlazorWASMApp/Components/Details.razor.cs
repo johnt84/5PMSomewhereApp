1dc74857acf076804c63fadfb2739901135da33b
John Tomlinson <john.tomlinson@teamsynergi.co.uk>
2024-03-21 12:54:50 +0000

    Added Mud Expansion panel around the details section

---
+++ FivePMSomewhereBlazorWASMApp/Components/Details.razor.cs
@@ -0,0 +1,17 @@
+﻿using FivePMSomewhereShared.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace FivePMSomewhereBlazorWASMApp.Components
+{
+    public partial class Details
+    {
+        [Parameter]
+        public TargetTimeModel? CurrentTimeZone { get; set; }
+
+        [Parameter]
+        public TimeAfterTargetModel? PreviousTimeZone { get; set; }
+
+        [Parameter]
+        public TimeBeforeTargetModel? NextTimeZone { get; set; }
+    }
+}
